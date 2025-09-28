da91f111ce9394fcacec8ec2a2d6b66e2d3370cb
kenduraghav <kenduraghav@gmail.com>
2025-09-28 23:15:34 +0530

    Fix User service client URL invoking issue

--- order-service/src/main/java/com/foodorder/order/client/UserServiceClient.java
+++ order-service/src/main/java/com/foodorder/order/client/UserServiceClient.java
@@ -2,6 +2,8 @@ package com.foodorder.order.client;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -12,6 +14,8 @@ import com.foodorder.order.dto.external.UserResponse;
 //User Service Validation using synchronous client.
 @Component
 public class UserServiceClient {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserServiceClient.class);
 	
 	private final RestTemplate restTemplate;
 	
@@ -28,7 +32,8 @@ public class UserServiceClient {
 		
 		try {
 			
-			String url = userServiceUrl + "api/users/" + userId;
+			String url = userServiceUrl + "/api/users/" + userId;
+			logger.info("UserServiceClient.getUserById() service url: " + url);
 			UserResponse userResponse = restTemplate.getForObject(url, UserResponse.class);
 			return Optional.ofNullable(userResponse);
 			
